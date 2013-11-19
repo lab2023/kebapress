79352cd7ba4086e37e9ec9e4b63a088375699295
Ahmet Sezgin Duran <marjinalist1@gmail.com>
2013-11-19 15:20:38 +0200

    #3 Fix show method in PostsController

--- app/controllers/kebapress/posts_controller.rb
+++ app/controllers/kebapress/posts_controller.rb
@@ -3,19 +3,17 @@ require_dependency "kebapress/application_controller"
 module Kebapress
   class PostsController < ApplicationController
     def index
-    	@posts = Post.all.where.not(published_at: nil)
+    	@posts = Kebapress::Post.all.where.not(published_at: nil)
     end
 
     def new
     end
 
     def show
+    	@post = Kebapress::Post.find(params[:id])
     end
 
     def edit
-    end
-
-    def show
     end
 
     def update
