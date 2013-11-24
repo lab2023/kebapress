6a86c0524e3680905977dd44874528b87f55165b
Ahmet Sezgin Duran <marjinalist1@gmail.com>
2013-11-24 15:36:14 +0200

    #3 Fix assets directory structure

--- app/assets/javascripts/kebapress/application.js
+++ app/assets/javascripts/kebapress/application.js
@@ -10,7 +10,7 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require_tree .
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap
+//= require_tree .
