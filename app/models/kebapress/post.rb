a2a11e09cc5be1f1fb917eb7f2d65e5b0e83d067
Ahmet Sezgin Duran <marjinalist1@gmail.com>
2013-12-09 13:01:53 +0200

    #13 Get uncategorized posts with scope

--- app/models/kebapress/post.rb
+++ app/models/kebapress/post.rb
@@ -1,6 +1,8 @@
 module Kebapress
   class Post < ActiveRecord::Base
     scope :published, -> { where(published: true) }
+    scope :uncategorized, -> { includes(:categories).where(kebapress_categories: { id: nil })}
+
     has_and_belongs_to_many :tags
     has_and_belongs_to_many :categories
 
@@ -11,7 +13,6 @@ module Kebapress
 
     def tag_list=(tags)
       self.tags.clear
-
       tags.split(",").each do |tag|
         self.tags << Kebapress::Tag.find_or_create_by_title(tag.strip)
       end
