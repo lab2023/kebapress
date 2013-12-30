59accc97dfa4050d9ae34288e24be7d68e854ffb
Ahmet Sezgin Duran <marjinalist1@gmail.com>
2013-12-30 16:59:51 +0200

    Update README.md for tutorial

--- app/models/kebapress/photo.rb
+++ app/models/kebapress/photo.rb
@@ -1,6 +1,5 @@
 module Kebapress
   class Photo < ActiveRecord::Base
-    has_attached_file :image,
-                      path: 'uploads/kebapress/images/:id/style/:id.:extension'
+    has_attached_file :image
   end
 end
